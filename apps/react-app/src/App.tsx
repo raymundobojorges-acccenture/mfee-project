c1e3686d30092b0c5e35e5424bfe50ce067dbdab
Jesus Saucedo <jesus.saucedo.ojeda@gmail.com>
2024-04-03 22:08:37 -0700

    Actividad 2

--- apps/react-app/src/App.tsx
+++ apps/react-app/src/App.tsx
@@ -1,29 +1,15 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid } from '@mui/material';
 
-import { Container } from './components/Header/Header.styles';
 import HomePage from './components/HomePage/HomePage'
 import PostPage from "./components/PostPage/PostPage"
+import Header from './components/Header/Header';
 
 
 function App() {
   return (
     <Grid container id="app" direction="column" height="100vh" flexWrap="nowrap">
-      {/* Activity 2 - Move all Container content to Header component */}
-      <Container container>
-        <Grid item flexGrow={1}>
-          <Typography variant="caption" color="primary" alignItems="center">
-            <span style={{ fontSize: '1.5rem' }}>[ </span>
-            Making your Life Easier
-            <span style={{ fontSize: '1.5rem' }}> ]</span>
-          </Typography>
-        </Grid>
-        <Grid item flexGrow={1}>
-          <Typography variant="h4" fontWeight="bold">
-            Discovering the World
-          </Typography>
-        </Grid>
-      </Container>
-
+      {/* DONE Activity 2 - Move all Container content to Header component */}
+      <Header/>
       <Grid item flexGrow={1}>
         {/* DONE Activity 1 - Render HomePage and PostPage */}
         <HomePage/>
--- apps/react-app/src/components/Header/Header.tsx
+++ apps/react-app/src/components/Header/Header.tsx
@@ -1,1 +1,28 @@
+import { Grid, Typography } from '@mui/material';
+
+import { Container } from './Header.styles';
+
+function Header() {
+  return (
+      <>
+        {/* DONE Activity 2 - Move all Container content to Header component */}
+        <Container container>
+        <Grid item flexGrow={1}>
+            <Typography variant="caption" color="primary" alignItems="center">
+            <span style={{ fontSize: '1.5rem' }}>[ </span>
+            Making your Life Easier
+            <span style={{ fontSize: '1.5rem' }}> ]</span>
+            </Typography>
+        </Grid>
+        <Grid item flexGrow={1}>
+            <Typography variant="h4" fontWeight="bold">
+            Discovering the World
+            </Typography>
+        </Grid>
+        </Container>
+    </>
+  );
+}
+
+export default Header;
 
