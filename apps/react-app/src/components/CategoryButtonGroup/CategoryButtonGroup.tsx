844d4d3bc157bcad726dc9ed81ee6175f95bfbfc
Jesus Saucedo <jesus.saucedo.ojeda@gmail.com>
2024-04-06 01:16:53 -0700

    error fixed

--- apps/react-app/src/components/CategoryButtonGroup/CategoryButtonGroup.tsx
+++ apps/react-app/src/components/CategoryButtonGroup/CategoryButtonGroup.tsx
@@ -32,24 +32,17 @@ function CategoryButtonGroup({ categorySelected, handleSelectCategory }: Categor
     <Container item>
       <ButtonGroup aria-label="category button group" color="inherit">
         { categoryOptions.map((category) => (
-          <>
-            {/*DONE Activity 5 - Iterate categoryOptions */}
-
-              <StyledButton key={category.key} type="button"
-              //DONE Activity 4 - Set a condition so that the props selected is true only if the option is selected. This value can be hardcoded in the following way: categoryOptions[0].name
-              // DONE Activity 5 - Replaces the hardcoded value "categoryOptions[0].name" with the variable obtained from the iteration and uses the variable "categorySelected" obtained from the props
-              selected={category.key === categorySelected}
-              onClick={() => {
-                handleSelectCategory(category.name);
-                // DONE Activity 5 - After you have iterated the "categoryOptions" array, send the "name" property as a parameter to the "handleSelectCategory" function
-              }}
-            >
-              {/* Activity 5 - Render category name */}
-              {/* DONE Activity 1 - Render category name */}
-                <label> {category.name} </label>
-              </StyledButton>
-          </>
-        )) }
+          <StyledButton
+            key={category.key}
+            type="button"
+            selected={category.key === categorySelected}
+            onClick={() => {
+              handleSelectCategory(category.name);
+            }}
+          >
+            <label>{category.name}</label>
+          </StyledButton>
+        ))}
       </ButtonGroup>
     </Container>
   );
