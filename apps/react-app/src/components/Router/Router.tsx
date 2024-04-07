25f828488656c251c01567a16caeff4499f8bb8b
Jesus Saucedo <jesus.saucedo.ojeda@gmail.com>
2024-04-06 22:29:55 -0700

    Activity 10 - Debido a temas de tiempo, no pude completar la navegación de la pagina, pero entiendo el concepto sin problema.

--- apps/react-app/src/components/Router/Router.tsx
+++ apps/react-app/src/components/Router/Router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Page, { HomePage } from "../Page";
+import PostPage from "../PostPage";
 
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <Page page={<HomePage />} />,
   },
+  {
+    path: "/post/:postId",
+    element: <PostPage/>
+  }
   // Activity 10 - Render the PostPage component with the dynamic postId and the NotFoundPage component as the default page in case of route errors
 ]);
 
