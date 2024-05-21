b0d0a761fe317a49aaa409e1ee845b08889e392c
Alejandro Lagarde <c-alejandro.lagarde@charter.com>
2024-05-21 00:50:02 -0600

    Actividad 1

---
+++ apps/vue-app/src/app/modules/posts/components/CategoryList.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- Inicio CategoriesList.vue -->
+  <div class="btn-group" role="group">
+    <CategoryItem />
+  </div>
+</template>
+
+<script>
+import CategoryItem from './CategoryItem.vue';
+
+export default {
+  components: {
+    CategoryItem
+  }
+};
+</script>
